71db9c5154ed034c07917be82c9003d9e76d537e
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2026-04-09 13:48:47 -0500

    Update TrackingMain.cpp

--- TrackingMain.cpp
+++ TrackingMain.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
 #include "Menus.h"
 using namespace std;
 
 int main() {
+	Menu groceries;
 	int selection = 0;
 
 	cin >> selection;
 
 	while ((selection >= 1) && (selection <= 4)) {
-		cout << "working" << endl;
-		break;
+		cout << "working. . ." << endl;
+
+		while (selection == 1) {
+			groceries.option1();
+			cout << "Make a new selection: ";
+			cin >> selection;
+		}
+
+		while (selection == 2) {
+			cout << "List placeholder" << endl;
+			break;
+		}
+
+		while (selection == 3) {
+			cout << "Histogram placeholder" << endl;
+			break;
+		}
+
+		while (selection == 4) {
+			cout << "Ending program. . ." << endl;
+			break;
+		}
 	}
 
 	return 0;
