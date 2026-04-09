f413d1a2c6abc0d3f50ba868b98d6711e4f342ee
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2026-04-09 14:54:40 -0500

    Update Menus.h
    added options and fixed string issue on m_item

--- Menus.h
+++ Menus.h
@@ -1,11 +1,22 @@
 #ifndef TRACKING_MAIN_MENUS_H_
 #define TRACKING_MAIN_MENUS_H_
 
+#include <string>
+
 class Menu {
 	public:
 		//default constructor
 		Menu();
 
+		void option1();
+		//void option2();
+		//void option3();
+		//void option4();
+
+	private:
+		int m_selection; //might not need. placeholder
+		std::string m_item;
+		int m_amount;
 
 };
 
