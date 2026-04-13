4054c29a099b05a505b88e4aacb6c50a4a58e4c5
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2026-04-13 13:33:24 -0500

    Update Menus.h
    added inputList()

--- Menus.h
+++ Menus.h
@@ -8,16 +8,19 @@ class Menu {
 		//default constructor
 		Menu();
 
-		void option1();
+		int option1();
 		//void option2();
 		//void option3();
 		//void option4();
 		void menuDisplay();
+		int inputList();
 
 	private:
 		int m_selection; //might not need. placeholder
-		std::string m_item;
+		std::string m_inputItem;
 		int m_amount;
+		std::string m_item;
+		int m_itemFrequency;
 
 };
 
