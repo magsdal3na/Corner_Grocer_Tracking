4d2476b8d6b92b41a9b8d8384222b230a7403085
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2026-04-13 13:32:59 -0500

    Update Menus.cpp
    added inFS for option 1 and kept template for other options

--- Menus.cpp
+++ Menus.cpp
@@ -1,22 +1,50 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <fstream>
 #include "Menus.h"
 using namespace std;
 
 Menu::Menu() {
 	m_selection = 0;
-	m_item = "";
+	m_inputItem = "";
 	m_amount = 0;
+	m_item = "";
+	m_itemFrequency = 0;
 }
 
 //prompt user for input
-void Menu::option1() {
-	cout << "Input the item name: ";
-	cin >> m_item;
-	cout << "Input the item's amount: ";
-	cin >> m_amount;
-	cout << m_item << " " << m_amount << endl;
+int Menu::option1() {
+	ifstream inFS;
+	string m_inputItem;
+	int m_itemFrequency = 0;
+	string m_item;
+
+	inFS.open("CS210_Project_Three_Input_File.txt");
+
+	if (!inFS.is_open()) {
+		cout << "Could not open file CS210_Project_Three_Input_File.txt." << endl;
+		return 1;
+	}
+
+	cout << "Enter an item's name: " << endl;
+	cin >> m_inputItem;
+
+	while (!inFS.eof()) {
+		inFS >> m_item;
+
+		if (!inFS.fail()) {
+			if (m_item == m_inputItem) {
+				++m_itemFrequency;
+			}
+		}
+	}
+
+	cout << m_inputItem << " " << m_itemFrequency << endl;
+
+	inFS.close();
+
+	return 0;
 }
 
 void Menu::menuDisplay() {
@@ -25,4 +53,32 @@ void Menu::menuDisplay() {
 	cout << setw(20) << "   Corner Grocer Tracking Program!   " << setw(20) << endl;
 	cout << setfill('-') << setw(40) << " " << endl;
 	cout << setfill(' ') << setw(20) << "Please select from one of these options: " << setw(20) << endl;
+}
+
+int Menu::inputList() {
+	ifstream inFS;
+	string m_inputItem;
+	int m_itemFrequency;
+	string m_item;
+
+	inFS.open("CS210_Project_Three_Input_File.txt");
+
+	if (!inFS.is_open()) {
+		cout << "Could not open file CS210_Project_Three_Input_File.txt." << endl;
+		return 1;
+	}
+
+	cout << "Printing item list. . ." << endl;
+
+	while (!inFS.eof()) {
+		inFS >> m_item;
+
+		if (!inFS.fail()) {
+			cout << m_item << endl;
+		}
+	}
+
+	inFS.close();
+
+	return 0;
 }
