4575e4c44285491f4c344b261da7b629fdca967e
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2026-04-17 13:52:48 -0500

    Update TrackingMain.cpp
    added full comments and changed repeated text

--- TrackingMain.cpp
+++ TrackingMain.cpp
@@ -6,33 +6,36 @@
 using namespace std;
 
 int main() {
-	Menu groceries;
+	Menu groceries; //sets up constructor
 	int selection = 0;
 
+	//creates backup file on startup
 	groceries.backupFileCreation();
 
+	//displays menu
 	groceries.menuDisplay();
 
 	cout << "Please select 1 - 4: ";
 	cin >> selection;
 
+	//iterates while input is a number within 1 - 4
 	while ((selection >= 1) && (selection <= 4)) {
 
 		while (selection == 1) {
 			groceries.option1();
-			cout << "Make a new selection: ";
+			cout << "Please select 1 - 4: ";
 			cin >> selection;
 		}
 
 		while (selection == 2) {
 			groceries.option2();
-			cout << "Make a new selection: ";
+			cout << "Please select 1 - 4: ";
 			cin >> selection;
 		}
 
 		while (selection == 3) {
 			groceries.option3();
-			cout << "Make a new selection: ";
+			cout << "Please select 1 - 4: ";
 			cin >> selection;
 		}
 
@@ -42,6 +45,8 @@ int main() {
 		}
 	}
 
+	//prints if user enters incorrect value
+	//exits program
 	cout << "Incorrect value entered. Please restart program." << endl;
 
 	return 0;
